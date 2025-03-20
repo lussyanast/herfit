fe31ae23d705986da52f64b6b51cf0a61bbf3782
lussyanast <lussyanast@gmail.com>
2025-03-20 16:01:24 +0700

    update dashboard

--- fitpoint-backend/app/Filament/Widgets/MonthlyTransactionsChart.php
+++ fitpoint-backend/app/Filament/Widgets/MonthlyTransactionsChart.php
@@ -3,20 +3,57 @@
 namespace App\Filament\Widgets;
 
 use Filament\Widgets\ChartWidget;
+use App\Models\Transaction;
+use Carbon\Carbon;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class MonthlyTransactionsChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?int $sort = 2;
+    protected static ?string $heading = 'Transaksi per Bulan';
 
     protected function getData(): array
     {
+        $startOfYear = Carbon::now()->startOfYear();
+        $endOfYear = Carbon::now()->endOfYear();
+
+        $transactions = Transaction::query()
+            ->whereBetween('created_at', [$startOfYear, $endOfYear])
+            ->get()
+            ->groupBy(function ($date) {
+                return Carbon::parse($date->created_at)->format('M');
+            });
+
+        $monthlyCounts = $transactions->mapWithKeys(function ($transactions, $month) {
+            return [
+                $month => count($transactions)
+            ];
+        });
+
+        $data = Trend::model(Transaction::class)
+            ->between(start: now()->startOfYear(), end: now()->endOfYear())
+            ->perMonth()
+            ->count();
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Transaksi per Bulan',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate)->values(),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date)->values(),
         ];
     }
 
     protected function getType(): string
     {
         return 'line';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Menampilkan jumlah transaksi per bulan selama tahun ini.';
     }
 }
