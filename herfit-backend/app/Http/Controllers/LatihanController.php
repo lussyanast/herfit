562c927a0e940a46dd9fed54272981f5a5babace
lussyanast <lussyanast@gmail.com>
2025-09-12 09:45:15 +0700

    Update LatihanController.php

--- herfit-backend/app/Http/Controllers/LatihanController.php
+++ herfit-backend/app/Http/Controllers/LatihanController.php
@@ -11,7 +11,7 @@ class LatihanController extends Controller
     public function index()
     {
         $latihan = Aktivitas::latihan()
-            ->where('id_pengguna', Auth::id())
+            ->where('id_pengguna', Auth::user()->id_pengguna ?? Auth::id())
             ->orderBy('tanggal', 'desc')
             ->get();
 
@@ -27,20 +27,30 @@ class LatihanController extends Controller
             'tanggal' => 'nullable|date',
         ]);
 
-        $data['id_pengguna'] = Auth::id();
+        $data['id_pengguna'] = Auth::user()->id_pengguna ?? Auth::id();
         $data['jenis_aktivitas'] = 'latihan';
         $data['tanggal'] = $data['tanggal'] ?? now()->toDateString();
 
-        $aktivitas = Aktivitas::create($data);
+        $lastId = Aktivitas::where('jenis_aktivitas', 'latihan')->max('id_aktivitas'); // contoh LAT023
+        $num = $lastId ? (int) substr($lastId, 3) + 1 : 1;
+        $data['id_aktivitas'] = 'LAT' . str_pad($num, 3, '0', STR_PAD_LEFT);
 
-        return response()->json($aktivitas, 201);
+        try {
+            $aktivitas = Aktivitas::create($data);
+            return response()->json($aktivitas, 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Gagal menyimpan template latihan',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function show($id)
     {
         $aktivitas = Aktivitas::latihan()
             ->where('id_aktivitas', $id)
-            ->where('id_pengguna', Auth::id())
+            ->where('id_pengguna', Auth::user()->id_pengguna ?? Auth::id())
             ->firstOrFail();
 
         return response()->json($aktivitas);
@@ -50,7 +60,7 @@ class LatihanController extends Controller
     {
         $aktivitas = Aktivitas::latihan()
             ->where('id_aktivitas', $id)
-            ->where('id_pengguna', Auth::id())
+            ->where('id_pengguna', Auth::user()->id_pengguna ?? Auth::id())
             ->firstOrFail();
 
         $aktivitas->delete();
